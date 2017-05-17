bcc4dd5c24cbf82fd3945ae4aa636c3d01629e44
Thiago Andrade <thiagoh@gmail.com>
2017-05-17 00:38:41 -0400

    first commit

---
+++ migrations/2_deploy_contracts.js
@@ -0,0 +1,13 @@
+// var ConvertLib = artifacts.require("./ConvertLib.sol");
+// var MetaCoin = artifacts.require("./MetaCoin.sol");
+var ProofOfExistence1 = artifacts.require("./ProofOfExistence1.sol");
+
+module.exports = function(deployer) {
+    // deployer.deploy(ConvertLib);
+    // deployer.link(ConvertLib, MetaCoin);
+    // deployer.deploy(MetaCoin);
+
+    deployer.deploy(ProofOfExistence1);
+};
+
+//var poe;ProofOfExistence1.deployed().then(function(instance){poe = instance;console.log(instance);});
