f2b6246c03c9addb37726709373b41eb8866991a
Thiago Andrade <thiagoh@gmail.com>
2017-05-19 18:12:33 -0400

    formatter configuration

--- mined-blocks.js
+++ mined-blocks.js
@@ -1,36 +1,36 @@
 function minedBlocks(lastn, addr) {
-    var addrs = [];
-    if (!addr) {
-        addr = eth.coinbase;
+  var addrs = [];
+  if (!addr) {
+    addr = eth.coinbase;
+  }
+  var limit = eth.blockNumber - lastn;
+
+  for (var i = eth.blockNumber; i >= limit && i >= 0; i--) {
+    var miner = eth.getBlock(i).miner;
+
+    if (eth.getBlock(i).miner === addr) {
+      addrs.push(i);
     }
-    var limit = eth.blockNumber - lastn;
+  }
 
-    for (var i = eth.blockNumber; i >= limit && i >= 0; i--) {
-        var miner = eth.getBlock(i).miner;
-
-        if (eth.getBlock(i).miner === addr) {
-            addrs.push(i);
-        }
-    }
-
-    return addrs;
+  return addrs;
 }
 
 function minedBlocks2(lastn, addr) {
-    var addrs = [];
-    if (!addr) {
-        addr = eth.coinbase;
+  var addrs = [];
+  if (!addr) {
+    addr = eth.coinbase;
+  }
+  var blockNumber = eth.syncing.currentBlock;
+  var limit = blockNumber - lastn;
+
+  for (var i = blockNumber; i >= limit && i >= 0; i--) {
+    var miner = eth.getBlock(i).miner;
+
+    if (eth.getBlock(i).miner === addr) {
+      addrs.push(i);
     }
-    var blockNumber = eth.syncing.currentBlock;
-    var limit = blockNumber - lastn;
+  }
 
-    for (var i = blockNumber; i >= limit && i >= 0; i--) {
-        var miner = eth.getBlock(i).miner;
-
-        if (eth.getBlock(i).miner === addr) {
-            addrs.push(i);
-        }
-    }
-
-    return addrs;
+  return addrs;
 }
